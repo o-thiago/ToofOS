aca0956d313cafefe1b300f96928fc5661cd8bb5
Thiago Macedo <119901046+o-thiago@users.noreply.github.com>
2026-04-27 16:31:48 -0400

    Add a simple game to test the pi

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, lib, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -9,16 +9,21 @@
     "flakes"
   ];
 
+  nixpkgs.config = {
+    allowUnfree = true;
+    # Allows nix-eval to run from unsupported architectures. Such as x86-64 linux desktops.
+    allowUnsupportedSystem = true;
+  };
+
   services.openssh.enable = true;
   networking = {
     hostName = "toofos";
-    networkmanager.enable = true;
-    networkmanager.wifi.powersave = false;
+    networkmanager = {
+      enable = true;
+      # Prevent host becoming unreachable on WiFi after some time.
+      wifi.powersave = false;
+    };
   };
-
-  # Required so NixOS includes the proprietary Raspberry Pi wireless firmware
-  # blobs needed by the onboard Wi-Fi/Bluetooth hardware.
-  hardware.enableRedistributableFirmware = true;
 
   time.timeZone = "UTC";
   i18n.defaultLocale = "pt_BR.UTF-8";
@@ -27,7 +32,8 @@
   users.users.gamer = {
     isNormalUser = true;
     description = "gamer";
-    initialPassword = "changeme";
+    initialPassword = "gamer";
+    shell = pkgs.fish;
     extraGroups = [
       "networkmanager"
       "wheel"
@@ -35,12 +41,28 @@
   };
 
   programs = {
+    gamescope.enable = true;
     git.enable = true;
+    fish.enable = true;
     neovim = {
       enable = true;
       defaultEditor = true;
     };
   };
+
+  hardware = {
+    graphics.enable = true;
+    raspberry-pi."4".fkms-3d.enable = true; # Vulkan based hardware acceleration.
+    # Required so NixOS includes the proprietary Raspberry Pi wireless firmware
+    # blobs needed by the onboard Wi-Fi/Bluetooth hardware.
+    enableRedistributableFirmware = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    (writeShellScriptBin "osu!" ''
+      exec ${lib.getExe gamescope} -- ${lib.getExe osu-lazer-bin} "$@"
+    '')
+  ];
 
   system.stateVersion = "26.05";
 }
