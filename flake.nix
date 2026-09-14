6773457b7be81104a517e4c58443ac6e7495bec4
Thiago Macedo <119901046+o-thiago@users.noreply.github.com>
2026-09-14 00:25:09 -0400

    Adiciona um formatador.

--- flake.nix
+++ flake.nix
@@ -4,17 +4,32 @@
   inputs = {
     nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";
     nixpkgs.follows = "nixos-raspberrypi/nixpkgs";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    inputs@{ nixos-raspberrypi, ... }:
-    {
-      nixosConfigurations.toofos = nixos-raspberrypi.lib.nixosSystem {
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          formatter = pkgs.nixfmt-tree;
+        };
+
+      flake.nixosConfigurations.toofos = inputs.nixos-raspberrypi.lib.nixosSystem {
         specialArgs = inputs;
         modules = [
           ./system/configuration.nix
           {
-            imports = with nixos-raspberrypi.nixosModules; [
+            imports = with inputs.nixos-raspberrypi.nixosModules; [
               raspberry-pi-4.base
               raspberry-pi-4.display-vc4
               raspberry-pi-4.bluetooth
